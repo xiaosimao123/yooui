03b87c5c470c42c5c58929e9892c877ebc208ac2
xjun4882 <xjun4882@gmail.com>
2024-05-22 10:37:04 +0800

    rm doc

---
+++ apps/docs/config/site.ts
@@ -0,0 +1,41 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "NextUI - Beautiful, fast and modern React UI Library",
+  description: "Make beautiful websites regardless of your design experience.",
+  ogImage: "https://YooUI.org/twitter-cards/YooUI.jpeg",
+  author: "Junior Garcia",
+  email: "jrgarciadev@gmail.com",
+  siteUrl: "https://YooUI.org",
+  creator: "@getnextui",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://YooUI.org",
+    siteName: "NextUI",
+    description: "Beautiful, fast and modern React UI Library",
+    images: [
+      {
+        url: "https://YooUI.org/twitter-cards/YooUI.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "NextUI",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "NextUI - Beautiful, fast and modern React UI Library",
+    description: "Make beautiful websites regardless of your design experience.",
+    image: "https://YooUI.org/twitter-cards/YooUI.jpeg",
+    creator: "@getnextui",
+  },
+  links: {
+    github: "https://github.com/xiaosimao123/yooui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui-docs-v2.vercel.app",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+    portfolio: "https://jrgarciadev.com",
+  },
+};
